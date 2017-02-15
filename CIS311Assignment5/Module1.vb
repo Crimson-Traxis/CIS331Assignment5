a67b138cab3967bbe67b8f358752d5d1d68d1c4a
Trent Killinger <trentkillinger@gmail.com>
2017-02-15 00:54:15 -0500

    Inital upload

---
+++ CIS311Assignment5/Module1.vb
@@ -0,0 +1,121 @@
+﻿Imports System.IO
+Imports System.Text
+
+Module Module1
+
+    Sub Main()
+        Dim studentList As List(Of Student) = New List(Of Student)
+        Dim data As String = ""
+        Console.WriteLine("Please specify location of file to load.")
+        Console.Write("Location: ")
+        Try
+            data = File.ReadAllText(Console.ReadLine())
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+        If (data <> "") Then
+            Dim reader As StringReader = New StringReader(data)
+            Dim line As String = reader.ReadLine()
+            While line <> Nothing
+                Try
+                    Dim args As String() = line.Split(" ")
+                    studentList.Add(New Student(args(0),
+                                                args(1),
+                                                Convert.ToSingle(args(2)),
+                                                Convert.ToSingle(args(3)),
+                                                Convert.ToSingle(args(4)),
+                                                Convert.ToSingle(args(5)),
+                                                Convert.ToSingle(args(6))))
+                Catch ex As Exception
+                    Console.WriteLine(ex.Message)
+                End Try
+                line = reader.ReadLine()
+            End While
+
+            Dim output As StringBuilder = New StringBuilder()
+            output.Append(Environment.NewLine)
+            output.AppendLine("                            Ye Old Country School")
+            output.AppendLine("                        *** Semester Grade Report ***")
+            output.AppendLine("                        -----------------------------")
+            output.Append(Environment.NewLine)
+            output.AppendLine("                         Homework Scores          Exam    Numeric  Letter")
+            output.AppendLine("      Name          1       2       3       4     Score   Grade    Grade")
+            output.AppendLine("--------------    -----   -----   -----   -----   ------  -------  ------")
+            Dim semesterReport = From students In studentList
+                                 Order By students.LastName Ascending
+                                 Select students
+            For Each student As Student In semesterReport
+                output.AppendLine(String.Format("{0,-5}{1,-13}{2,-5:#.00}{3,8:#.00}{4,8:#.00}{5,8:#.00}{6,9:#.00}{7,9:#.00}     {8,-2}",
+                                                student.Initials,
+                                                student.LastName,
+                                                student.Homework1,
+                                                student.Homework2,
+                                                student.Homework3,
+                                                student.Homework4,
+                                                student.ExamScore,
+                                                student.NumericGrade,
+                                                student.LetterGrade))
+            Next
+            output.AppendLine("-------------------------------------------------------------------------")
+            output.AppendLine("                      Grade Distribution Statistics")
+            output.AppendLine("-------------------------------------------------------------------------")
+            output.AppendLine("")
+            Dim gradeList As List(Of String) = {"A", "B", "C", "D", "F"}.ToList()
+            For Each grade As String In gradeList
+                output.AppendLine("Those students earning a " + grade + " grade are:")
+                Dim gradeReport = From students In studentList
+                                  Order By students.LastName Ascending
+                                  Select students Where students.LetterGrade(0) = grade
+                For Each student As Student In gradeReport
+                    output.AppendLine(String.Format("{0,-5}{1,-13}--> {2,-6:#.00}", student.Initials, student.LastName, student.LetterGrade))
+                Next
+                If (gradeReport.Count = 0) Then
+                    output.AppendLine("None")
+                End If
+                output.AppendLine("")
+            Next
+            output.AppendLine("-------------------------------------------------------------------------")
+            output.AppendLine("                 Homework/Exam Grade Range Statistics")
+            output.AppendLine("-------------------------------------------------------------------------")
+            output.AppendLine("")
+            Dim HW1 = New With {
+                          Key .min = studentList.Min(Function(x) x.Homework1),
+                          Key .max = studentList.Max(Function(x) x.Homework1),
+                          Key .avg = studentList.Average(Function(x) x.Homework1)
+                      }
+            Dim HW2 = New With {
+                          Key .min = studentList.Min(Function(x) x.Homework2),
+                          Key .max = studentList.Max(Function(x) x.Homework2),
+                          Key .avg = studentList.Average(Function(x) x.Homework2)
+                      }
+            Dim HW3 = New With {
+                          Key .min = studentList.Min(Function(x) x.Homework3),
+                          Key .max = studentList.Max(Function(x) x.Homework3),
+                          Key .avg = studentList.Average(Function(x) x.Homework3)
+                      }
+            Dim HW4 = New With {
+                          Key .min = studentList.Min(Function(x) x.Homework4),
+                          Key .max = studentList.Max(Function(x) x.Homework4),
+                          Key .avg = studentList.Average(Function(x) x.Homework4)
+                      }
+            Dim Exam = New With {
+                          Key .min = studentList.Min(Function(x) x.ExamScore),
+                          Key .max = studentList.Max(Function(x) x.ExamScore),
+                          Key .avg = studentList.Average(Function(x) x.ExamScore)
+                      }
+            Dim highestGrade = From students In studentList
+                               Order By students.NumericGrade Descending
+                               Select students
+            output.AppendLine("The highest course grade of " + highestGrade.First().NumericGrade.ToString("#.00") + " was eraned by:")
+            output.AppendLine(String.Format("{0,-5}{1,-13}--> {2,-6:#.00}", highestGrade.First().Initials, highestGrade.First().LastName, highestGrade.First().NumericGrade))
+            output.AppendLine("")
+            Dim lowestGrade = From students In studentList
+                              Order By students.NumericGrade Ascending
+                              Select students
+            output.AppendLine("The lowest course grade of " + lowestGrade.First().NumericGrade.ToString("#.00") + " was eraned by:")
+            output.AppendLine(String.Format("{0,-5}{1,-13}--> {2,-6:#.00}", lowestGrade.First().Initials, lowestGrade.First().LastName, lowestGrade.First().NumericGrade))
+            Console.WriteLine(output.ToString())
+        End If
+    End Sub
+
+End Module
---
+++ CIS311Assignment5/Student.vb
@@ -0,0 +1,151 @@
+﻿Public Class Student
+    Private _initials As String
+    Private _lastName As String
+    Private _homework1 As Single
+    Private _homework2 As Single
+    Private _homework3 As Single
+    Private _homework4 As Single
+    Private _examScore As Single
+    Private _numericGrade As Single
+    Private _letterGrade As String
+
+    Public Sub New(initials As String,
+                   lastName As String,
+                   homework1 As Single,
+                   homework2 As Single,
+                   homework3 As Single,
+                   homework4 As Single,
+                   examScore As Single)
+        _initials = initials
+        _lastName = lastName
+        _homework1 = homework1
+        _homework2 = homework2
+        _homework3 = homework3
+        _homework4 = homework4
+        _examScore = examScore
+        CalculateNumericGrade()
+        CalculateLetterGrade()
+    End Sub
+
+    Public Sub New()
+        _initials = ""
+        _lastName = ""
+        _homework1 = 0
+        _homework2 = 0
+        _homework3 = 0
+        _homework4 = 0
+    End Sub
+
+    Public Sub CalculateNumericGrade()
+        _numericGrade = ((_homework1 + _homework2 + _homework3 + _homework4) * 0.5) + ((_examScore) * 0.5)
+    End Sub
+
+    Public Sub CalculateLetterGrade()
+        If (_numericGrade >= 95) Then
+            _letterGrade = "A"
+        ElseIf (_numericGrade >= 90 And _numericGrade < 95) Then
+            _letterGrade = "A-"
+        ElseIf (_numericGrade >= 87 And _numericGrade < 90) Then
+            _letterGrade = "B+"
+        ElseIf (_numericGrade >= 84 And _numericGrade < 87) Then
+            _letterGrade = "B"
+        ElseIf (_numericGrade >= 80 And _numericGrade < 84) Then
+            _letterGrade = "B-"
+        ElseIf (_numericGrade >= 77 And _numericGrade < 80) Then
+            _letterGrade = "C+"
+        ElseIf (_numericGrade >= 74 And _numericGrade < 77) Then
+            _letterGrade = "C"
+        ElseIf (_numericGrade >= 70 And _numericGrade < 74) Then
+            _letterGrade = "C-"
+        ElseIf (_numericGrade >= 67 And _numericGrade < 70) Then
+            _letterGrade = "D+"
+        ElseIf (_numericGrade >= 64 And _numericGrade < 67) Then
+            _letterGrade = "D"
+        ElseIf (_numericGrade >= 60 And _numericGrade < 64) Then
+            _letterGrade = "D-"
+        Else
+            _letterGrade = "F"
+        End If
+    End Sub
+
+    Public Property Initials() As String
+        Get
+            Return _initials
+        End Get
+        Set(ByVal value As String)
+            _initials = value
+        End Set
+    End Property
+
+    Public Property LastName() As String
+        Get
+            Return _lastName
+        End Get
+        Set(ByVal value As String)
+            _lastName = value
+        End Set
+    End Property
+
+    Public Property Homework1() As Single
+        Get
+            Return _homework1
+        End Get
+        Set(ByVal value As Single)
+            _homework1 = value
+        End Set
+    End Property
+
+    Public Property Homework2() As Single
+        Get
+            Return _homework2
+        End Get
+        Set(ByVal value As Single)
+            _homework2 = value
+        End Set
+    End Property
+
+    Public Property Homework3() As Single
+        Get
+            Return _homework3
+        End Get
+        Set(ByVal value As Single)
+            _homework3 = value
+        End Set
+    End Property
+
+    Public Property Homework4() As Single
+        Get
+            Return _homework4
+        End Get
+        Set(ByVal value As Single)
+            _homework4 = value
+        End Set
+    End Property
+
+    Public Property ExamScore() As Single
+        Get
+            Return _examScore
+        End Get
+        Set(ByVal value As Single)
+            _examScore = value
+        End Set
+    End Property
+
+    Public Property NumericGrade() As Single
+        Get
+            Return _numericGrade
+        End Get
+        Set(ByVal value As Single)
+            _numericGrade = value
+        End Set
+    End Property
+
+    Public Property LetterGrade() As String
+        Get
+            Return _letterGrade
+        End Get
+        Set(ByVal value As String)
+            _letterGrade = value
+        End Set
+    End Property
+End Class
